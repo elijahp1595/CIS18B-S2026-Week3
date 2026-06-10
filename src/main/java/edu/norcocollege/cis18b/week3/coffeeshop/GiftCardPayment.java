7a144e4c661837dff8ac78ae6a90d535ccae77ea
elijahp1595 <pina.elijah@gmail.com>
2026-06-10 05:19:21 +0000

    I finished the gift card file

---
+++ src/main/java/edu/norcocollege/cis18b/week3/coffeeshop/GiftCardPayment.java
@@ -0,0 +1,28 @@
+package edu.norco.cis18b.week3.coffeeshop;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public class GiftCardPayment implements PaymentMethod {
+    private BigDecimal balance;
+
+    public GiftCardPayment(BigDecimal initialBalance) {
+        if (initialBalance == null || initialBalance.compareTo(BigDecimal.ZERO) < 0){
+            throw new IllegalArgumentException("Balance must be >= 0");
+        }
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public PaymentReceipt pay(String orderId, BigDecimal amount) {
+        if (balance.compareTo(amount) < 0) {
+            throw new IllegalArgumentException("Insufficent balance");
+        }
+        balance = balance.subtract(amount);
+        return new PaymentReceipt(orderId, amount, "GIFT_CARD", Instant.now());
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+}
