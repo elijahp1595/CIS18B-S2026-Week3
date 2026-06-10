78c559cfad1fe28741df1ed9972fe0547202c937
elijahp1595 <pina.elijah@gmail.com>
2026-06-10 04:59:47 +0000

    finished payment files

---
+++ src/main/java/edu/norcocollege/cis18b/week3/coffeeshop/CreditCardPayment.java
@@ -0,0 +1,21 @@
+package edu.norco.cis18b.week3.coffeeshop;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public class CreditCardPayment implements PaymentMethod {
+    private String last4;
+
+    public CreditCardPayment(String last4) {
+        if (last4 == null || last4.length() != 4) {
+            throw new IllegalArgumentException("last4 must be exactly 4 characters");
+        }
+        this.last4 = last4;
+    }
+
+    @Override
+    public PaymentReceipt pay(String orderId, BigDecimal amount) {
+        String method = "CREDIT_CARD(***" + last4 + ")";
+        return new PaymentReceipt(orderId, amount, method, Instant.now());
+    }
+}
---
+++ src/main/java/edu/norcocollege/cis18b/week3/coffeeshop/PaymentReceipt.java
@@ -0,0 +1,7 @@
+package edu.norco.cis18b.week3.coffeeshop;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record PaymentReceipt(String orderId, BigDecimal amount, String method, Instant timestamp) {
+}
